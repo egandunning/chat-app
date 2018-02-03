0e40808921df0d20d036d710d7df36f972768460
egandunning <dunninea@plu.edu>
2018-02-02 21:08:34 -0800

    client connects to server using websockets from index.html

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 
 
 const port = process.env.PORT || 3000;
 const fs = require('fs');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(port, () => {
+//register an event listener
+io.on('connection', socket => {
+   console.log('new user connected');
+
+   socket.on('disconnect', socket => {
+      console.log('user disconnected');
+   });
+});
+
+server.listen(port, () => {
    console.log(`server started on port ${port}`)
 });
