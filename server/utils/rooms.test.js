58001bcd7f26307ee2147193b60aacd816b68284
Egan Dunning <dunninea@plu.edu>
2018-02-18 14:27:05 -0500

    added test cases

--- server/utils/rooms.test.js
+++ server/utils/rooms.test.js
@@ -26,14 +26,45 @@ describe('Rooms', () => {
 
    describe('Remove room', () => {
       it('should delete a room', () => {
-         expect(rooms.removeRoom('room1')).toEqual({name:'room1'});
+         const room = {name:'room1', password:''};
+         expect(rooms.removeRoom('room1')).toEqual(room);
          expect(rooms.rooms.length).toBe(1);
+      });
+   });
+
+   describe('Get all rooms', () => {
+      it('should get list of all room names', () => {
+         const roomNames = ['room1', 'room2'];
+         expect(rooms.getRooms()).toEqual(roomNames);
+      });
+   });
+
+   describe('Get room by name', () => {
+      it('should get a room object', () => {
+         const room = {name: 'room1', password: ''};
+         const foundRoom = rooms.getRoom('room1');
+         expect(foundRoom).toEqual(room);
+      });
+
+      it('should return undefined if room isn\'t found', () => {
+         expect(rooms.getRoom('room3')).toBe(undefined);
       });
    });
 
    describe('Determine if room can be joined', () => {
       it('should be able to join room with matching name, password', () => {
-         //let rooms = 
+         const room = rooms.getRoom('room1');
+         expect(rooms.canJoin('room1', '', room)).toBe(true);
+      });
+
+      it('shouldn\'t be able to join room without matching password', () => {
+         const room = rooms.getRoom('room1');
+         expect(rooms.canJoin('room1', 'password', room)).toBe(false);
+      });
+
+      it('shouldn\'t be able to join room that doesn\'t exist', () => {
+         const room = rooms.getRoom('room3');
+         expect(rooms.canJoin('room3', '', room)).toBe(false);
       });
    });
 });
