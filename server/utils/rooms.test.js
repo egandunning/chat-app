9f81fa7ef6982219627528c6946a257a60ec12ae
Egan Dunning <dunninea@plu.edu>
2018-02-18 13:38:20 -0500

    simplified test cases by adding beforeEach function

--- server/utils/rooms.test.js
+++ server/utils/rooms.test.js
@@ -2,29 +2,38 @@ const expect = require('expect');
 
 const {Rooms} = require('./rooms');
 
-describe('Add room', () => {
-   it('should add a new room', () => {
-      let rooms = new Rooms();
-      rooms.addRoom('room1');
-      expect(rooms.rooms.length).toBe(1);
-      rooms.addRoom('room2');
-      expect(rooms.rooms.length).toBe(2);
-   });
-
-   it('should not add duplicate rooms', () => {
-      let rooms = new Rooms();
-      rooms.addRoom('room1');
-      rooms.addRoom('room1');
-      expect(rooms.rooms.length).toBe(1);
-   });
-});
-
-describe('Remove room', () => {
-   it('should delete a room', () => {
-      let rooms = new Rooms();
+describe('Rooms', () => {
+   
+   let rooms;
+   beforeEach(() => {
+      rooms = new Rooms();
       rooms.addRoom('room1');
       rooms.addRoom('room2');
-      expect(rooms.removeRoom('room1')).toEqual({name:'room1'});
-      expect(rooms.rooms.length).toBe(1);
+   });
+   
+   describe('Add room', () => {
+      it('should add a new room', () => {
+         expect(rooms.rooms.length).toBe(2);
+         rooms.addRoom('room3');
+         expect(rooms.rooms.length).toBe(3);
+      });
+
+      it('should not add duplicate rooms', () => {
+         rooms.addRoom('room1');
+         expect(rooms.rooms.length).toBe(2);
+      });
+   });
+
+   describe('Remove room', () => {
+      it('should delete a room', () => {
+         expect(rooms.removeRoom('room1')).toEqual({name:'room1'});
+         expect(rooms.rooms.length).toBe(1);
+      });
+   });
+
+   describe('Determine if room can be joined', () => {
+      it('should be able to join room with matching name, password', () => {
+         //let rooms = 
+      });
    });
 });
