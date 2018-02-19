4e6c559dfb0059ed71ab8d9511610eb5b8255be1
Egan Dunning <dunninea@plu.edu>
2018-02-19 06:50:27 -0500

    added documentation generation script and removed unneccessary file

--- server/utils/joinroom.js
+++
@@ -1,9 +0,0 @@
-const {isRealString} = require('./validators');
-
-const join = (name, room, password) => {
-   if(!isRealString(name) || !isRealString(room)) {
-      return callback('Display name and room name must not be blank');
-   }
-
-   if(password != 
-
